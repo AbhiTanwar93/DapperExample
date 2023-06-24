b3625d80ca37ae8d00f1eaafc3d76e9d6de4d090
AbhiTanwar93 <tanwarabhi1993@gmail.com>
2023-06-25 00:21:52 +0530

    Add project files.

---
+++ Dapper.Business/Implementation/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.Business.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Business.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IProductRepository productRepository)
+        {
+            Products = productRepository;
+        }
+        public IProductRepository Products { get; }
+    }
+}
---
+++ Dapper.Business/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Dapper.Business.Contract;
+using Dapper.Business.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.Business
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection RegisterService(this IServiceCollection services)
+        {
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
